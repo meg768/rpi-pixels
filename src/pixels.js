d7146e5c5f43c14f01ee060c5f3877f35acd7ade
meg768 <magnus@egelberg.se>
2025-09-25 22:50:24 +0200

    -

--- src/pixels.js
+++ src/pixels.js
@@ -1,91 +1,135 @@
 var Color = require('color');
 
-function debug() {
-}
-
 module.exports = class Pixels {
+	constructor(options) {
+		options = options || {};
 
-        constructor(options) {
+		if (options.width == undefined || options.height == undefined) {
+			throw new Error('Width and height must be specified.');
+		}
 
-			options = options || {};
+		this.width = options.width;
+		this.height = options.height;
+		this.pixels = new Uint32Array(this.width * this.height);
+		this.map = null;
 
-			if (options.width == undefined || options.height == undefined)
-	            throw new Error('Width and height must be specified.');
+		if (options.map instanceof Uint32Array && options.map.length == this.width * this.height) {
+			this.map = options.map;
+		}
 
-			this.width = options.width;
-			this.height = options.height;
-			this.pixels = new Uint32Array(this.width * this.height);
-			this.contents = new Uint32Array(this.width * this.height);
-			this.tmp = new Uint32Array(this.width * this.height);
+		if (options.map === 'serpentine') {
+			let map = new Uint32Array(this.width * this.height);
 
-			if (typeof options.debug === 'function') {
-				debug = options.debug;
+			for (var i = 0; i < map.length; i++) {
+				var row = Math.floor(i / this.width);
+				var col = i % this.width;
+
+				if (row % 2 === 0) {
+					map[i] = i;
+				} else {
+					map[i] = (row + 1) * this.width - (col + 1);
+				}
 			}
-			else if (options.debug) {
-				debug = console.log;
-            }
-            this.Color = Color;
-	
-        }
 
-        
-        RGB(red, green, blue) {        
-            return ((red << 16) | (green << 8) | blue);
-        }
-
-        HSL(h, s, l) {
-            return Color.hsl(h, s, l).rgbNumber();
-        }
-
-		color(name) {
-			return Color(name).rgbNumber();
+			this.map = map;
 		}
 
-        fill(color) {
-            if (typeof color == 'string')
-                color = Color(color).rgbNumber();
-    
-            for (var i = 0; i < this.pixels.length; i++)
-                this.pixels[i] = color;
-        }
+		if (!this.map) {
+			let map = new Uint32Array(this.width * this.height);
 
-        fillRGB(r, g, b) {
-            this.fill(this.RGB(r, g, b));
-        }
-
-        fillHSL(h, s, l) {
-            this.fill(this.HSL(h, s, l));
-        }
-
-        clear() {
-            this.fill(0);
-        }
-
-		setPixel(x, y, color) {
-            this.pixels[y * this.width + x] = color;
-        }
-
-        getPixel(x, y) {
-            return this.pixels[y * this.width + x];
+			for (var i = 0; i < map.length; i++) {
+				map[i] = i;
+			}
+			this.map = map;
 		}
-		
-        setPixelColor(x, y, color) {
-            this.pixels[y * this.width + x] = color;
-        }
+	}
 
-        getPixelColor(x, y) {
-            return this.pixels[y * this.width + x];
-        }
-    
-        setPixelRGB(x, y, red, green, blue) {
-            this.pixels[y * this.width + x] = this.RGB(red, green, blue);
-        }
- 
-        setPixelHSL(x, y, h, s, l) {
-            this.pixels[y * this.width + x] = this.HSL(h, s, l);
+	RGB(red, green, blue) {
+		return (red << 16) | (green << 8) | blue;
+	}
+
+	HSL(h, s, l) {
+		return Color.hsl(h, s, l).rgbNumber();
+	}
+
+	color(name) {
+		return Color(name).rgbNumber();
+	}
+
+	fill(color) {
+		if (typeof color == 'string') color = Color(color).rgbNumber();
+
+		for (var i = 0; i < this.pixels.length; i++) {
+			this.pixels[i] = color;
 		}
-		
-		render(options) {
-        }
- 
-}
+	}
+
+	getIndex(x, y) {
+		return this.map[y * this.width + x];
+	}
+
+	fillRGB(r, g, b) {
+		this.fill(this.RGB(r, g, b));
+	}
+
+	fillHSL(h, s, l) {
+		this.fill(this.HSL(h, s, l));
+	}
+
+	clear() {
+		this.fill(0);
+	}
+
+	setPixel(x, y, color) {
+		this.pixels[this.getIndex(x, y)] = color;
+	}
+
+	getPixel(x, y) {
+		return this.pixels[this.getIndex(x, y)];
+	}
+
+	setPixelColor(x, y, color) {
+		this.pixels[this.getIndex(x, y)] = color;
+	}
+
+	getPixelColor(x, y) {
+		return this.pixels[this.getIndex(x, y)];
+	}
+
+	setPixelRGB(x, y, red, green, blue) {
+		this.pixels[this.getIndex(x, y)] = this.RGB(red, green, blue);
+	}
+
+	setPixelHSL(x, y, h, s, l) {
+		this.pixels[this.getIndex(x, y)] = this.HSL(h, s, l);
+	}
+
+	static gammaCorrect(pixels, gamma = 2.2) {
+		if (!(pixels instanceof Uint32Array)) {
+			throw new Error('pixels must be a Uint32Array of 0xRRGGBB values');
+		}
+		if (typeof gamma !== 'number' || gamma <= 0) {
+			return pixels;
+		}
+
+		const output = new Uint32Array(pixels.length);
+
+		for (let i = 0; i < pixels.length; i++) {
+			const rgb = pixels[i] >>> 0; // 0xRRGGBB
+
+			const r1 = (rgb >>> 16) & 0xff;
+			const g1 = (rgb >>> 8) & 0xff;
+			const b1 = (rgb >>> 0) & 0xff;
+
+			const r2 = Math.min(255, Math.round(Math.pow(r1 / 255, gamma) * 255));
+			const g2 = Math.min(255, Math.round(Math.pow(g1 / 255, gamma) * 255));
+			const b2 = Math.min(255, Math.round(Math.pow(b1 / 255, gamma) * 255));
+
+			output[i] = ((r2 << 16) | (g2 << 8) | b2) >>> 0;
+		}
+
+		return output;
+	}
+
+	render(options) {}
+};
